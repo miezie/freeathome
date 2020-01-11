a804ffc2ec2a2987f47ff0f3f10c1bcc011b8aa9
Minze Tolsma <minze@MacBook-Pro-van-Minze.local>
2020-01-11 20:20:16 +0100

    call edit

--- freeathome/__init__.py
+++ freeathome/__init__.py
@@ -32,6 +32,8 @@ _LOGGER = logging.getLogger(__name__)
 async def async_setup(hass, config):
     hass.async_create_task(
             hass.config_entries.flow.async_init(
+                DOMAIN,
+                context={"source": config_entries.SOURCE_IMPORT},
                 data={
                     "config": config.get(DOMAIN)
                 },
